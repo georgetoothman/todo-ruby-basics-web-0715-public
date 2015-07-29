0b183efbdcba9e18a6a4f237f2b0a1466bdcf9a0
George Toothman <george.toothman@gmail.com>
2015-07-29 17:57:29 -0400

    pass all tests

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(argument)
+  argument = "I'm right and you are wrong!"
 end
 
-def greeting
-
+def greeting(greeting, name)
+  greeting = "Hi there, "
+  name = "Bobby!"
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  "I"
+  "am"
+  "a"
+  "ruby"
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+  topping
 end
